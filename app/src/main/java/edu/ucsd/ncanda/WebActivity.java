9f7e139b7f6fb5b44c89fa0363941b1474f21b91
michaelcmkay <euromkay@gmail.com>
2016-08-04 22:15:53 -0700

    initial commit

---
+++ app/src/main/java/edu/ucsd/ncanda/WebActivity.java
@@ -0,0 +1,28 @@
+package edu.ucsd.ncanda;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+
+public class WebActivity extends Activity {
+
+    private WebView mWebview ;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        String url = "https://secure.projopts.org/ncanda_ema1/subjects/token.php?token=" + FirebaseInstanceId.getInstance().getToken();
+
+        mWebview  = new WebView(this);
+        mWebview.getSettings().setJavaScriptEnabled(true); // enable javascript
+        mWebview.setWebViewClient(new WebViewClient());
+        mWebview .loadUrl(url);
+        setContentView(mWebview );
+    }
+}
