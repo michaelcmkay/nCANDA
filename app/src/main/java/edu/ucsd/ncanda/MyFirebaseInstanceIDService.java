907e1d991edb35f33258a47777ddd8f6309a7994
michaelcmkay <euromkay@gmail.com>
2016-08-11 13:50:29 -0700

    Fixed Log null bug

--- app/src/main/java/edu/ucsd/ncanda/MyFirebaseInstanceIDService.java
+++ app/src/main/java/edu/ucsd/ncanda/MyFirebaseInstanceIDService.java
@@ -1,5 +1,10 @@
 package edu.ucsd.ncanda;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
 import com.google.firebase.iid.FirebaseInstanceId;
@@ -12,13 +17,18 @@ public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
 
 
     public void onTokenRefresh() {
-        // Get updated InstanceID token.
         String refreshedToken = FirebaseInstanceId.getInstance().getToken();
-        Log.d(TAG, "Refreshed token: " + refreshedToken);
 
-        // If you want to send messages to this application instance or
-        // manage this apps subscriptions on the server side, send the
-        // Instance ID token to your app server.
+        Log.d("FCN TOKEN GET", "Refreshed token: " + refreshedToken);
+
+        final Intent intent = new Intent("tokenReceiver");
+        // You can also include some extra data.
+        final LocalBroadcastManager broadcastManager = LocalBroadcastManager.getInstance(this);
+        intent.putExtra("token",refreshedToken);
+        broadcastManager.sendBroadcast(intent);
+
     }
+
+
 
 }
--- app/src/main/java/edu/ucsd/ncanda/WebActivity.java
+++ app/src/main/java/edu/ucsd/ncanda/WebActivity.java
@@ -1,6 +1,11 @@
 package edu.ucsd.ncanda;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,6 +14,7 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Toast;
 
+import com.google.firebase.crash.FirebaseCrash;
 import com.google.firebase.iid.FirebaseInstanceId;
 
 public class WebActivity extends Activity {
@@ -16,15 +22,35 @@ public class WebActivity extends Activity {
     private WebView mWebview ;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        String url = "https://secure.projopts.org/ncanda_ema1/subjects/token.php?token=" + FirebaseInstanceId.getInstance().getToken();
+        LocalBroadcastManager.getInstance(this).registerReceiver(tokenReceiver,
+                new IntentFilter("tokenReceiver"));
+        setWebView();
+    }
+    private void setWebView(){
+        String token = FirebaseInstanceId.getInstance().getToken();
+        if(token == null) {
+            Toast.makeText(this, "Please wait while your token is being generated!", Toast.LENGTH_LONG).show();
+            Log.d("WEB_ACTIVITY", "was null");
+            return;
+        }
+        String url = "https://secure.projopts.org/ncanda_ema1/subjects/token.php?token=" + token;
 
         mWebview  = new WebView(this);
         mWebview.getSettings().setJavaScriptEnabled(true); // enable javascript
         mWebview.setWebViewClient(new WebViewClient());
-        mWebview .loadUrl(url);
+        mWebview.loadUrl(url);
         setContentView(mWebview );
 
-        Log.d("WEB_ACTIVITY", FirebaseInstanceId.getInstance().getToken());
+        Log.d("WEB_ACTIVITY", token);
     }
+
+    BroadcastReceiver tokenReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String token = intent.getStringExtra("token");
+            if(token != null)
+                setWebView();
+
+
+        }
+    };
 }
