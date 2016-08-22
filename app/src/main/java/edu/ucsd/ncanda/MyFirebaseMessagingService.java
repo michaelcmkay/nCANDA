6b4cc4cd20e751e70ce046b6e0cd8f83424ff4f9
michaelcmkay <euromkay@gmail.com>
2016-08-22 14:42:17 -0700

    logo icon fix

--- app/src/main/java/edu/ucsd/ncanda/MyFirebaseMessagingService.java
+++ app/src/main/java/edu/ucsd/ncanda/MyFirebaseMessagingService.java
@@ -55,7 +55,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
         //Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
-                .setSmallIcon(R.mipmap.ic_launcher)
+                .setSmallIcon(R.drawable.notif)
                 .setContentTitle(title)
                 .setContentText(messageBody)
                 .setAutoCancel(true)
